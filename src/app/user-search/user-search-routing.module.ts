9a5c9c1c8154316e150f2b892fd4bf9ba9e6333d
hawkgs <hawk.dvp@gmail.com>
2019-08-07 00:20:30 +0300

    feat(user-search): component routing

--- src/app/user-search/user-search-routing.module.ts
+++ src/app/user-search/user-search-routing.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { UserSearchComponent } from './user-search.component';
+import { SearchInputComponent } from './search-input/search-input.component';
+import { ResultsListComponent } from './results-list/results-list.component';
 
 
-const routes: Routes = [
-  { path: '', component: UserSearchComponent }
-];
+const routes: Routes = [{
+  path: '',
+  component: UserSearchComponent,
+  children: [{
+    path: '',
+    pathMatch: 'full',
+    component: SearchInputComponent
+  }, {
+    path: 'search',
+    component: ResultsListComponent
+  }]
+}];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
