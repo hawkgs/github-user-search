c6849b21789eb358913f1286864c6f17f8b49f41
hawkgs <hawk.dvp@gmail.com>
2019-08-07 21:53:15 +0300

    feat(search-input): implement functionality

--- src/app/user-search/search-input/search-input.component.ts
+++ src/app/user-search/search-input/search-input.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'gus-search-input',
   templateUrl: './search-input.component.html',
   styleUrls: ['./search-input.component.css']
 })
-export class SearchInputComponent implements OnInit {
+export class SearchInputComponent {
+  searchString: string;
 
-  constructor() { }
+  constructor(private router: Router) { }
 
-  ngOnInit() {
+  onSearch() {
+    const queryParams = this.searchString
+      .split(' ')
+      .map((param: string) => {
+        const paramArr = param.trim().split(':');
+        if (paramArr.length === 1) {
+          return { param: 'username', value: paramArr[0] };
+        }
+        return { param: paramArr[0], value: paramArr[1] };
+      })
+      .reduce((obj: any, { param, value }: any) => {
+        obj[param] = value;
+        return obj;
+      }, {});
+
+    queryParams.page = 1;
+
+    this.router.navigate(['/search'], { queryParams });
   }
-
 }
